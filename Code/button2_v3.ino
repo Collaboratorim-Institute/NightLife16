0caba792f7912dc4704873c35257a5b7d3fb36e3
William Felker <hello@williamfelker.com>
2016-11-12 16:43:15 -0800

    ADDED media docs and web app

---
+++ Code/button2_v3.ino
@@ -0,0 +1,12 @@
+/************************
+* CLASS | Objects and Space
+* PROJECT |
+* VERION | 1.3
+* NAME |
+************************/
+
+
+// initiate elements
+// On boot count down to start
+// on start start counting buttons presses unitl counter of one button hits 50.
+// Flash to stop and display winner.
